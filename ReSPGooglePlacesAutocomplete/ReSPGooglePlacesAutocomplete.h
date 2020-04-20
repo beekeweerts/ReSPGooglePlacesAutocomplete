f045b0ee89eaa03bc61e272bfadac645cfeb338f
Beeke Weerts <beeke@simpletechs.net>
2020-04-20 10:13:47 +0200

    .

---
+++ ReSPGooglePlacesAutocomplete/ReSPGooglePlacesAutocomplete.h
@@ -0,0 +1,19 @@
+//
+//  ReSPGooglePlacesAutocomplete.h
+//  ReSPGooglePlacesAutocomplete
+//
+//  Created by Beeke Weerts on 20.04.20.
+//  Copyright © 2020 ekappug. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ReSPGooglePlacesAutocomplete.
+FOUNDATION_EXPORT double ReSPGooglePlacesAutocompleteVersionNumber;
+
+//! Project version string for ReSPGooglePlacesAutocomplete.
+FOUNDATION_EXPORT const unsigned char ReSPGooglePlacesAutocompleteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReSPGooglePlacesAutocomplete/PublicHeader.h>
+
+
